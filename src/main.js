d732c5b1f357ffdacd0fd1661472ffff941f558e
Eccsx <thomasmattone83@outlook.fr>
2021-06-18 21:11:48 +0200

    📦 Jest

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ const game = new Game();
 
 console.log(game);
 
-game.enableStrategyLayout();
+// game.enableStrategyLayout();
 
 // res -> https://www.worldothello.org/ratings/player?searchPlayerInput=SCHOTTE+Tom
 const match = 'f5f6e6f4e3c5g5g3g4f3g6d3c4h6h5h4c2d7d2d6e7d8c8c6e8h7h2f8g8f7b6b5a6a4f2c7a5a7b8c3b3b4a3a2b2b1e2f1e1a1g2h1h3g1h8b7g7d1c1';
 
-// game.loadTranscript(match);
+//game.loadTranscript(match);
